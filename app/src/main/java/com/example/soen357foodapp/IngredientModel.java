280b6deed39d12638790a7304a2a4c3b69959468
David-Salomon Dahan <ddahan008@gmail.com>
2022-11-24 11:11:49 -0500

    update IngredientModel to generate its own IDs

--- app/src/main/java/com/example/soen357foodapp/IngredientModel.java
+++ app/src/main/java/com/example/soen357foodapp/IngredientModel.java
@@ -6,15 +6,18 @@ package com.example.soen357foodapp;
  * Creation: 2022-11-23
  */
 public class IngredientModel {
+    public static int iCount; // tracks the number of ingredients in the system
+
     public int iid; // ingredient ID
 
     public String iname; // ingredient name
 
     public int iqtty; // ingredient quantity
 
-    public IngredientModel(int iid, String iname, int iqtty) {
-        this.iid = iid;
+    public IngredientModel(String iname, int iqtty) {
+        this.iid = iCount;
         this.iname = iname;
         this.iqtty = iqtty;
+        iCount++;
     }
 }
